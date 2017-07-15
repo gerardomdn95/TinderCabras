b36932959a8b1fd3fc0c5fb7898ce7dfdaa78c24
Carlos Correa Mandujano <carloscorrea.dev@gmail.com>
2017-07-14 21:03:05 -0500

    reparando rutas

--- src/app/appComponent/appCtrl.js
+++ src/app/appComponent/appCtrl.js
@@ -1,14 +1,15 @@
-(function () {
-    'use strict';
+(function(){
+  'use strict';
 
-    angular
-        .module('myApp')
-        .component('appComponent', {
-            templateUrl: 'src/app/appComponent/appComponent.html',
-            controller: appCtrl,
-            controllerAs: 'vm'
-        });
+  angular
+    .module('myApp')
+    .component('appComponent',{
+      templateUrl: 'src/app/appComponent/appComponent.html',
+      controller: appCtrl,
+      controllerAs: 'vm'
+    });
+
     function appCtrl() {
-        var vm = this;
+      var vm = this;
     }
 })();
